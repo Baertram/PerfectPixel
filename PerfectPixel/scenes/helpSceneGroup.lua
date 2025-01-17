b5eaeeb5286785d2de98d03c379a016b3492d943
Baertram <pz1@gmx.net>
2025-01-17 23:59:44 +0100

    -Fixed help tutorial scrollbar style

--- PerfectPixel/scenes/helpSceneGroup.lua
+++ PerfectPixel/scenes/helpSceneGroup.lua
@@ -3,9 +3,9 @@ local removeFragmentsFromScene = PP.removeFragmentsFromScene
 
 PP.helpSceneGroup = function()
 	local scenes = {
-		{ scene = SCENE_MANAGER:GetScene('helpTutorials'), gVar = HELP, },
-		{ scene = HELP_EMOTES_SCENE,                       gVar = KEYBOARD_PLAYER_EMOTE, },
-		{ scene = HELP_CUSTOMER_SUPPORT_SCENE,             gVar = HELP_CUSTOMER_SUPPORT_KEYBOARD, },
+		{ scene = SCENE_MANAGER:GetScene('helpTutorials'), fragment = HELP_TUTORIALS_FRAGMENT, 	gVar = HELP, },
+		{ scene = HELP_EMOTES_SCENE,                       										gVar = KEYBOARD_PLAYER_EMOTE, },
+		{ scene = HELP_CUSTOMER_SUPPORT_SCENE,             										gVar = HELP_CUSTOMER_SUPPORT_KEYBOARD, },
 	}
 	local fragments	= { FRAME_PLAYER_FRAGMENT, FRAME_TARGET_STANDARD_RIGHT_PANEL_FRAGMENT, RIGHT_BG_FRAGMENT, TREE_UNDERLAY_FRAGMENT, TITLE_FRAGMENT, HELP_TITLE_FRAGMENT, }
 
@@ -20,5 +20,18 @@ PP.helpSceneGroup = function()
 		PP:CreateBackground(tlc, --[[#1]] nil, nil, nil, -10, -10, --[[#2]] nil, nil, nil, 0, 10)
 		PP.Anchor(tlc, --[[#1]] TOPRIGHT, GuiRoot, TOPRIGHT, 0, 120, --[[#2]] true, BOTTOMRIGHT, GuiRoot, BOTTOMRIGHT, 0, -70)
 	end
+
+	local helpTutorialObj = scenes[1].gVar -- HELP
+	local helpTutorialFragment = scenes[1].fragment -- HELP_TUTORIALS_FRAGMENT
+
+	local helpTutorialXPBarChanged = false
+	helpTutorialFragment:RegisterCallback("StateChange", function(oldState, newState)
+        if newState == SCENE_FRAGMENT_SHOWN and not helpTutorialXPBarChanged then
+			PP.ScrollBar(helpTutorialObj.navigationTree.scrollControl)
+			helpTutorialXPBarChanged = true
+        --elseif newState == SCENE_FRAGMENT_HIDDEN then
+        end
+    end)
+
 end
 

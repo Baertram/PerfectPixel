0aebb5ea0929ab1892aece1da94783ea4957d322
Cullen Daugherty <dakjaniels@outlook.com>
2026-02-18 00:32:35 -0600

    feat: refactor mailSceneGroup

--- PerfectPixel/scenes/mailSceneGroup.lua
+++ PerfectPixel/scenes/mailSceneGroup.lua
@@ -1,136 +1,161 @@
-local PP = PP ---@class PP
+local PP = PP --- @class PP
+local removeFragmentsFromScene = PP.removeFragmentsFromScene
 
-PP.mailSceneGroup = function()
+-- Constants for fragment removal
+local FRAGMENTS_TO_REMOVE =
+{
+    FRAME_PLAYER_FRAGMENT,
+    RIGHT_BG_FRAGMENT,
+    TITLE_FRAGMENT,
+    MAIL_TITLE_FRAGMENT,
+}
 
-	local SV = PP.savedVars.ListStyle
+-- Helper function to edit Mail Inbox scene
+-- MAIL_INBOX object uses navigationTree (ZO_Tree) for both categories and mail entries
+local function Edit_MailInbox(SV)
+    local navigationTree = MAIL_INBOX.navigationTree -- ZO_Tree
+    local navigationContainer = navigationTree.scrollControl
 
---MAIL_INBOX--MAIL_INBOX_SCENE----------------------------------------------------------------------
-	local navigationTree		= MAIL_INBOX.navigationTree --ZO_Tree
-	local navigationContainer	= navigationTree.scrollControl
+    -- Remove default fragments for clean UI
+    removeFragmentsFromScene(MAIL_INBOX_SCENE, FRAGMENTS_TO_REMOVE)
 
-	MAIL_INBOX_SCENE:RemoveFragment(FRAME_PLAYER_FRAGMENT)
-	MAIL_INBOX_SCENE:RemoveFragment(RIGHT_BG_FRAGMENT)
-	MAIL_INBOX_SCENE:RemoveFragment(TITLE_FRAGMENT)
-	MAIL_INBOX_SCENE:RemoveFragment(MAIL_TITLE_FRAGMENT)
+    -- Create custom background and anchoring for main inbox window
+    PP:CreateBackground(ZO_MailInbox, --[[#1]] nil, nil, nil, -10, -10, --[[#2]] nil, nil, nil, 0, 10)
+    PP.Anchor(ZO_MailInbox, --[[#1]] TOPRIGHT, GuiRoot, TOPRIGHT, 0, 120, --[[#2]] true, BOTTOMRIGHT, GuiRoot, BOTTOMRIGHT, 0, -70)
 
-	PP:CreateBackground(ZO_MailInbox, --[[#1]] nil, nil, nil, -10, -10, --[[#2]] nil, nil, nil, 0, 10)
+    -- Reposition inbox elements
+    PP.Anchor(ZO_MailInboxDeleteOnClaim, --[[#1]] BOTTOMLEFT, ZO_MailInboxInventoryUsage, TOPLEFT, 0, -4)
+    PP.Anchor(ZO_MailInboxInventoryUsage, --[[#1]] BOTTOMLEFT, ZO_MailInbox, BOTTOMLEFT, 0, 4)
+    PP.Anchor(navigationContainer, --[[#1]] TOPLEFT, ZO_MailInbox, TOPLEFT, 0, 90, --[[#2]] true, BOTTOMLEFT, ZO_MailInboxDeleteOnClaim, TOPLEFT, 0, 0)
 
-	PP.Anchor(ZO_MailInbox,		--[[#1]] TOPRIGHT,	GuiRoot,		TOPRIGHT,	0, 120,	--[[#2]] true, BOTTOMRIGHT,		GuiRoot,		BOTTOMRIGHT,	0, -70)
+    -- Apply custom scrollbar styling
+    PP.ScrollBar(navigationContainer, --[[sb_c]] 180, 180, 180, 0.7, --[[bd_c]] 20, 20, 20, 0.7, false)
+    ZO_Scroll_SetMaxFadeDistance(navigationContainer, SV.list_fade_distance)
 
-	PP.Anchor(ZO_MailInboxDeleteOnClaim, --[[#1]] BOTTOMLEFT,	ZO_MailInboxInventoryUsage,	TOPLEFT,	0, -4	--[[#2]])
-	PP.Anchor(ZO_MailInboxInventoryUsage, --[[#1]] BOTTOMLEFT,	ZO_MailInbox,	BOTTOMLEFT,	0, 4	--[[#2]])
-	PP.Anchor(navigationContainer,	--[[#1]] TOPLEFT,	ZO_MailInbox,	TOPLEFT,	0, 90,	--[[#2]] true, BOTTOMLEFT,		ZO_MailInboxDeleteOnClaim,	TOPLEFT,	0, 0)
-	PP.ScrollBar(navigationContainer,	--[[sb_c]] 180, 180, 180, 0.7, --[[bd_c]] 20, 20, 20, 0.7, false)
-	ZO_Scroll_SetMaxFadeDistance(navigationContainer, SV.list_fade_distance)
+    -- Hook into tree template setup to customize mail row controls
+    local mailRowTemplate = navigationTree.templateInfo["ZO_MailInboxRow"]
+    if mailRowTemplate then
+        local existingSetupCallback = mailRowTemplate.setupFunction
+        mailRowTemplate.setupFunction = function (node, control, mailData, open)
+            existingSetupCallback(node, control, mailData, open)
+        end
 
-------------------------------------------------------------------------------------------
-    --local function HighlightControl(control, animateInstantly)
---		control.backdrop:SetEdgeColor(SV.list_skin_edge_sel_col[1], SV.list_skin_edge_sel_col[2], SV.list_skin_edge_sel_col[3], SV.list_skin_edge_sel_col[4])
---    end
+        -- Hook the pool factory to apply styling when controls are created
+        local pool = mailRowTemplate.objectPool
+        if pool then
+            local existingFactory = pool.m_Factory
+            pool.m_Factory = function (objectPool)
+                local control = existingFactory(objectPool)
 
-    --local function UnhighlightControl(control, animateInstantly)
---		control.backdrop:SetEdgeColor(SV.list_skin_edge_col[1], SV.list_skin_edge_col[2], SV.list_skin_edge_col[3], SV.list_skin_edge_col[4])
---    end
+                -- Style the mail row control on creation
+                control:SetHeight(SV.list_control_height)
 
-	-- self, self.control, self.data, self.open, userRequested, self.tree:IsEnabled()
+                -- Clear default background texture
+                local bg = control:GetNamedChild("BG")
+                if bg then
+                    bg:SetTexture("PerfectPixel/tex/tex_clear.dds")
+                end
 
-	-- local mailEntry = tree["templateInfo"]["ZO_MailInboxRow"]
-	-- local existingSetupCallback = mailEntry.setupFunction
-	-- mailEntry.setupFunction = function(node, control, mailData, open)
-		-- existingSetupCallback(node, control, data, open)
+                -- Reposition and resize icon
+                local icon = control:GetNamedChild("Icon")
+                if icon then
+                    PP.Anchor(icon, --[[#1]] LEFT, control, LEFT, 5, 0)
+                    icon:SetDimensions(26, 26)
+                    icon:SetDrawLayer(1)
+                end
 
-		-- control.HighlightControl		= HighlightControl
-		-- control.UnhighlightControl		= UnhighlightControl
-	-- end
+                -- Style subject label
+                local subject = control:GetNamedChild("TextContainer"):GetNamedChild("Subject")
+                if subject then
+                    PP.Font(subject, --[[Font]] PP.f.u57, 16, "outline", --[[Alpha]] nil, --[[Color]] nil, nil, nil, nil, --[[StyleColor]] 0, 0, 0, .5)
+                end
 
+                return control
+            end
+        end
+    end
 
-    -- navigationTree:AddTemplate("ZO_MailInboxHeader", TreeHeaderSetup, NO_SELECTED_CALLBACK, CategoryEqualityFunction, TREE_CHILD_INDENT, CHILD_SPACING)
-    -- navigationTree:AddTemplate("ZO_MailInboxRow", MailEntrySetup, MailEntryOnSelected, MailEqualityFunction)
-    -- navigationTree:AddTemplate("ZO_MailInboxEmptyRow", MailEmptyEntrySetup, NO_SELECTED_CALLBACK)
+    -- Mail rows use the existing HighlightControl/UnhighlightControl system
+    -- No need to hook EnterRow/ExitRow - the tree handles it via Row_OnMouseEnter/Exit
 
-    -- self.templateInfo[template] =
-    -- {
-        -- template = template,
-        -- childIndent = childIndent,
-        -- childSpacing = childSpacing,
-        -- setupFunction = setupFunction,
-        -- selectionFunction = selectionFunction,
-        -- equalityFunction = equalityFunction or DefaultEqualityFunction,
-        -- objectPool = ZO_ControlPool:New(template, self.control)
-    -- }
+    -- Style the message pane scrollbar (right side where you read mail)
+    local messagePane = ZO_MailInboxMessagePane
+    if messagePane then
+        PP.ScrollBar(messagePane, --[[sb_c]] 180, 180, 180, 0.7, --[[bd_c]] 20, 20, 20, 0.7, false)
+        ZO_Scroll_SetMaxFadeDistance(messagePane, SV.list_fade_distance)
+    end
 
+    -- Hide default message background textures for cleaner look
+    local messageBGLeft = ZO_MailInboxMessageBGLeft
+    local messageBGRight = ZO_MailInboxMessageBGRight
+    if messageBGLeft then messageBGLeft:SetHidden(true) end
+    if messageBGRight then messageBGRight:SetHidden(true) end
 
+    -- Style message labels with custom fonts
+    local messageFrom = ZO_MailInboxMessageFrom
+    local messageSubject = ZO_MailInboxMessageSubject
+    local messageBody = ZO_MailInboxMessageBody
 
-	-- local function RefreshControlMode_1(control, typeId)
-		-- control:SetHeight(SV.list_control_height)
-		-- control:GetNamedChild("BG"):SetTexture("PerfectPixel/tex/tex_clear.dds")
+    if messageFrom then
+        PP.Font(messageFrom, --[[Font]] PP.f.u67, 18, "outline", --[[Alpha]] nil, --[[Color]] nil, nil, nil, nil, --[[StyleColor]] 0, 0, 0, 0.5)
+    end
+    if messageSubject then
+        PP.Font(messageSubject, --[[Font]] PP.f.u67, 20, "outline", --[[Alpha]] nil, --[[Color]] nil, nil, nil, nil, --[[StyleColor]] 0, 0, 0, 0.5)
+    end
+    if messageBody then
+        PP.Font(messageBody, --[[Font]] PP.f.u67, 16, "soft-shadow-thin", --[[Alpha]] nil, --[[Color]] nil, nil, nil, nil, --[[StyleColor]] 0, 0, 0, 0.5)
+    end
+end
 
-		-- if typeId == 1 then
-			-- PP:CreateBgToSlot(control)
+-- Helper function to edit Mail Send scene
+local function Edit_MailSend()
+    -- Remove default fragments for clean UI
+    removeFragmentsFromScene(MAIL_SEND_SCENE, FRAGMENTS_TO_REMOVE)
 
-			-- local icon = control:GetNamedChild("Icon")
-			-- PP.Anchor(icon, --[[#1]] LEFT, control, LEFT, 5, 0)
-			-- icon:SetDimensions(26, 26)
-			-- icon:SetDrawLayer(1)
+    -- Create custom background and anchoring for send mail window
+    PP:CreateBackground(ZO_MailSend, --[[#1]] nil, nil, nil, -10, -10, --[[#2]] nil, nil, nil, 0, 6)
 
-			-- local subject = control:GetNamedChild("Subject")
-			-- PP.Font(subject, --[[Font]] PP.f.u57, 16, "outline", --[[Alpha]] nil, --[[Color]] nil, nil, nil, nil, --[[StyleColor]] 0, 0, 0, .5)
-			-- PP.Anchor(subject, --[[#1]] LEFT, control:GetNamedChild("Icon"), RIGHT, 5, 0)
-		-- end
-	-- end
+    -- Hide inventory background when mail send is open
+    PP:HideBackgroundForScene(MAIL_SEND_SCENE, ZO_PlayerInventory.PP_BG)
 
-	-- local function RefreshControlMode_1_Dynamic(control, data, typeId)
-		-- if data.sortIndex == mailList.selectedDataIndex then
-			-- control.backdrop:SetEdgeColor(SV.list_skin_edge_sel_col[1], SV.list_skin_edge_sel_col[2], SV.list_skin_edge_sel_col[3], SV.list_skin_edge_sel_col[4])
-		-- else
-			-- control.backdrop:SetEdgeColor(SV.list_skin_edge_col[1], SV.list_skin_edge_col[2], SV.list_skin_edge_col[3], SV.list_skin_edge_col[4])
-		-- end
-	-- end
+    -- Position the send mail window
+    PP.Anchor(ZO_MailSend, --[[#1]] TOPRIGHT, GuiRoot, TOPRIGHT, 0, 120, --[[#2]] true, BOTTOMRIGHT, GuiRoot, BOTTOMRIGHT, 0, -90)
 
-	-- ZO_Scroll_SetMaxFadeDistance(mailList, SV.list_fade_distance)
+    -- Style input field labels
+    local toLabel = ZO_MailSendToLabel
+    local subjectLabel = ZO_MailSendSubjectLabel
+    local attachLabel = ZO_MailSendAttachMoneyLabel
+    local codLabel = ZO_MailSendCoDLabel
+    local postageLabel = ZO_MailSendPostageLabel
+    local attachmentsHeader = ZO_MailSendAttachmentsHeader
 
-	-- mailList.refreshControlMode_1			= RefreshControlMode_1
-	-- mailList.refreshControlMode_1_dynamic	= RefreshControlMode_1_Dynamic
-	-- mailList.uniformControlHeight			= SV.list_uniform_control_height
-	-- mailList.highlightTemplate				= nil
-	-- mailList.selectionTemplate				= nil
+    if toLabel then
+        PP.Font(toLabel, --[[Font]] PP.f.u67, 20, "outline", --[[Alpha]] nil, --[[Color]] nil, nil, nil, nil, --[[StyleColor]] 0, 0, 0, 0.5)
+    end
+    if subjectLabel then
+        PP.Font(subjectLabel, --[[Font]] PP.f.u67, 20, "outline", --[[Alpha]] nil, --[[Color]] nil, nil, nil, nil, --[[StyleColor]] 0, 0, 0, 0.5)
+    end
+    if attachLabel then
+        PP.Font(attachLabel, --[[Font]] PP.f.u67, 16, "outline", --[[Alpha]] nil, --[[Color]] nil, nil, nil, nil, --[[StyleColor]] 0, 0, 0, 0.5)
+    end
+    if codLabel then
+        PP.Font(codLabel, --[[Font]] PP.f.u67, 16, "outline", --[[Alpha]] nil, --[[Color]] nil, nil, nil, nil, --[[StyleColor]] 0, 0, 0, 0.5)
+    end
+    if postageLabel then
+        PP.Font(postageLabel, --[[Font]] PP.f.u67, 16, "outline", --[[Alpha]] nil, --[[Color]] nil, nil, nil, nil, --[[StyleColor]] 0, 0, 0, 0.5)
+    end
+    if attachmentsHeader then
+        PP.Font(attachmentsHeader, --[[Font]] PP.f.u67, 18, "outline", --[[Alpha]] nil, --[[Color]] nil, nil, nil, nil, --[[StyleColor]] 0, 0, 0, 0.5)
+    end
+end
 
-	-- for typeId in pairs(mailList.dataTypes) do
-		-- if typeId == 1 or typeId == 2 then
-			-- local dataType = ZO_ScrollList_GetDataTypeTable(mailList, typeId)
-			-- local pool = dataType.pool
+-- Main mail scene group function
+PP.mailSceneGroup = function ()
+    local SV = PP.savedVars.ListStyle
 
-			-- if dataType.height then
-				-- dataType.height = SV.list_control_height
-			-- end
+    -- Edit Mail Inbox Scene
+    Edit_MailInbox(SV)
 
-			-- PP.Hook_m_Factory(dataType, function(control)
-				-- mailList.refreshControlMode_1(control, typeId)
-			-- end)
-
-			-- if typeId == 1 then
-				-- PP.Hook_SetupCallback(dataType, function(control, data)
-					-- mailList.refreshControlMode_1_dynamic(control, data, typeId)
-				-- end)
-			-- end
-		-- end
-	-- end
-
-	-- ZO_PreHook(MAIL_INBOX, "EnterRow", function(self, control)
-		-- control.backdrop:SetCenterColor(SV.list_skin_backdrop_hl_col[1], SV.list_skin_backdrop_hl_col[2], SV.list_skin_backdrop_hl_col[3], SV.list_skin_backdrop_hl_col[4])
-	-- end)
-	-- ZO_PreHook(MAIL_INBOX, "ExitRow", function(self, control)
-		-- control.backdrop:SetCenterColor(SV.list_skin_backdrop_col[1], SV.list_skin_backdrop_col[2], SV.list_skin_backdrop_col[3], SV.list_skin_backdrop_col[4])
-	-- end)
-
---mailSend--MAIL_SEND_SCENE------------------------------------------------------------------------
-	MAIL_SEND_SCENE:RemoveFragment(FRAME_PLAYER_FRAGMENT)
-	MAIL_SEND_SCENE:RemoveFragment(RIGHT_BG_FRAGMENT)
-	MAIL_SEND_SCENE:RemoveFragment(TITLE_FRAGMENT)
-	MAIL_SEND_SCENE:RemoveFragment(MAIL_TITLE_FRAGMENT)
-
-	PP:CreateBackground(ZO_MailSend, --[[#1]] nil, nil, nil, -10, -10, --[[#2]] nil, nil, nil, 0, 6)
-	PP:HideBackgroundForScene(MAIL_SEND_SCENE, ZO_PlayerInventory.PP_BG)
-
-	PP.Anchor(ZO_MailSend, --[[#1]] TOPRIGHT,	GuiRoot, TOPRIGHT, 0, 120, --[[#2]] true, BOTTOMRIGHT, GuiRoot, BOTTOMRIGHT, 0, -90)
+    -- Edit Mail Send Scene
+    Edit_MailSend()
 end
